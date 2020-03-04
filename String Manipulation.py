c5f39844dee5ee215eb556ac4b2f909c9d247b3c
Tom <55069697+tclap27@users.noreply.github.com>
2020-03-04 18:36:40 -0500

    Add files via upload

---
+++ String Manipulation.py
@@ -0,0 +1,8 @@
+name = "Tom Clappsy"
+color = "green"
+animal = "penguin"
+team = "Yankees"
+
+
+print("My name is " + name + ", my favorite color is " + color +
+    ", my favorite animal is a " + animal + ", my favorite team is the " + team)
